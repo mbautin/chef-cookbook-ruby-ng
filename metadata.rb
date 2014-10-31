5ad116618c2077f83de6ddc968df8fc7f8cd3851
Maciej Pasternacki <maciej@3ofcoins.net>
2014-10-31 13:16:13 +0100

    Actually bump version in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'maciej@3ofcoins.net'
 license          'MIT'
 description      'Installs Ruby from brightbox/ruby-ng PPA'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.3.0'
 
 supports 'ubuntu'
 
